1607f3d60602b06d72422b345e4b338ce18c3dcb
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2026-09-06 23:09:34 +0200

    devc: add from i.mx8 board
    Signed-off-by: Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>

---
+++ bsp/qnx800.bsp.hw.st_stm32mp257f-dk/bsp/BSP_st-stm32mp257f-dk_be-800/src/hardware/dwmac/readme.txt
@@ -0,0 +1,2 @@
+https://github.com/torvalds/linux/blob/master/Documentation/devicetree/bindings/net/snps%2Cdwmac.yaml
+https://github.com/torvalds/linux/blob/2f0c1cf72f4682178506f513bbf015e591b1aa4a/drivers/net/ethernet/stmicro/stmmac/dwmac-stm32.c
---
+++ bsp/qnx800.bsp.hw.st_stm32mp257f-dk/bsp/BSP_st-stm32mp257f-dk_be-800/src/hardware/startup/lib/public/soc/st/stm32mp2/include/stm32mp257.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2026 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You
+ * may not reproduce, modify or distribute this software except in
+ * compliance with the License. You may obtain a copy of the License
+ * at: http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ *
+ * This file may contain contributions from others, either as
+ * contributors under the License or as licensors under other terms.
+ * Please review this entire file for other proprietary rights or license
+ * notices, as well as the QNX Development Suite License Guide at
+ * http://licensing.qnx.com/license-guide/ for other information.
+ */
+
+
+#ifndef STM32MP257_H_
+#define STM32MP257_H_
+
+/** STM32MP2 SoC cores number */
+#define STM32MP2_CPU_CORES_NUMBER  2
+
+/* STM32MP25 RCC Register base address RM0457 Rev 5 243/5881 */
+#define STM32MP2_RCC_BASE_ADDR    (0x44200000UL)
+
+/** STM32MP2 USART2 base address and size */
+#define STM32_USART2_BASE_ADDR        (0x400e0000UL)  /* RM0457 Rev 5, page 248/5881 */
+#define STM32_UART_SIZE               (0x400UL)  /* RM0457 Rev 5, page 248/5881 */
+#define STM32MP2_UART2_IRQ            (147)  /* UART2 IRQ number for EXTI1 */
+#define STM32MP2_UART2_EXTI1_IRQ      (27)  /* UART2 IRQ number for EXTI1 */
+#define STM32MP2_UART_CLOCK_FREQ      (64000000UL)   /* UART2 clock frequency in Hz */
+
+/** Device electronic signature registers (RM0457 Rev 5 5837/5881) */
+#define STM32MP2_BSEC_BASE          (0x44000000UL)
+#define STM32MP2_BSEC_FVR5          (STM32MP2_BSEC_BASE + 0x014UL)  /* Unique device ID register (96 bits) (UID) */
+#define STM32MP2_BSEC_FVR6          (STM32MP2_BSEC_BASE + 0x018UL)
+#define STM32MP2_BSEC_FVR7          (STM32MP2_BSEC_BASE + 0x01CUL)
+#define STM32MP2_BSEC_FVR9          (STM32MP2_BSEC_BASE + 0x024UL)  /* Device part number (RPN) */
+#define STM32MP2_BSEC_FVR102        (STM32MP2_BSEC_BASE + 0x198UL)  /* Device version */
+#define STM32MP2_BSEC_FVR122        (STM32MP2_BSEC_BASE + 0x1E8UL)  /* Package data register (PKG) */
+
+#define STM32MP2_UID_SIZE           96  /* in bits */
+#define STM32MP2_UID_SIZE_BYTES     (STM32MP2_UID_SIZE / 8)
+
+/** STM32MP2 GIC addresses */
+#define GICD_PADDR            (0x4AC10000UL)
+#define GICC_PADDR            (0x4AC20000UL)
+#define GICH_PADDR            (0x4AC40000UL)
+#define GICV_PADDR            (0x4AC60000UL)
+
+/** STM32MP2 independent-watchdog addresses */
+#define STM32MP2_IWDG1_BASE   (0x44010000UL)
+#define STM32MP2_IWDG2_BASE   (0x44020000UL)
+#define STM32MP2_IWDG3_BASE   (0x44030000UL)
+#define STM32MP2_IWDG4_BASE   (0x44040000UL)
+
+#define STM32MP2_ETH1_MAC_BASE    (0x482C0000UL)
+#define STM32MP2_ETH1_MAC_SIZE    (0x4000UL)  /* 16KB */
+#define STM32MP2_ETH1_MAC_IRQ     (130)  /* Example IRQ number for ETH1 */
+
+#if defined(__QNXNTO__) && defined(__USESRCVERSION)
+#include <sys/srcversion.h>
+__SRCVERSION("$URL: http://svn.ott.qnx.com/product/hardware/branches/release/hardware/startup/lib/public/soc/st/stm32mp2/include/stm32mp257.h $ $Rev: 985114 $")
+#endif
+
+#endif  /* STM32MP257_H_ */
